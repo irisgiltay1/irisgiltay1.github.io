5541764b2dcbca6327b45cae951c7be1e3d40222
irisgiltay1 <gilt0001@hz.nl>
2023-09-07 21:22:21 +0200

    created faq along with its js

---
+++ main.js
@@ -0,0 +1,17 @@
+
+
+var faq = document.getElementsByClassName("faq-question");
+var i;
+
+for (i = 0; i < faq.length; i++) {
+    faq[i].addEventListener("click", function () {
+        this.classList.toggle("selected");
+
+        var body = this.nextElementSibling;
+        if (body.style.display === "block") {
+            body.style.display = "none";
+        } else {
+            body.style.display = "block";
+        }
+    });
+}
