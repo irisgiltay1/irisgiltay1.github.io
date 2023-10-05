9728ea61a5a5146b171171e8a4a8b554502daec6
irisgiltay1 <gilt0001@hz.nl>
2023-10-05 12:51:45 +0200

    fixes

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
-var faq = document.getElementsByClassName("faq-question");
-var i;
+const faq = document.getElementsByClassName("faq-question");
 
 for (i = 0; i < faq.length; i++) {
   faq[i].addEventListener("click", function () {
     this.classList.toggle("selected");
 
-    var body = this.nextElementSibling;
+    const body = this.nextElementSibling;
     if (body.style.display === "block") {
       body.style.display = "none";
   } else {
