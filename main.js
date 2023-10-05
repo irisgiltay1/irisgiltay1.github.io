5ff65b59f788df0ef14d5a6ccecf96e5bb1d05fc
irisgiltay1 <gilt0001@hz.nl>
2023-10-05 12:15:24 +0200

    changes

--- main.js
+++ main.js
@@ -2,14 +2,14 @@ var faq = document.getElementsByClassName("faq-question");
 var i;
 
 for (i = 0; i < faq.length; i++) {
-    faq[i].addEventListener("click", function () {
-        this.classList.toggle("selected");
+  faq[i].addEventListener("click", function () {
+    this.classList.toggle("selected");
 
-        var body = this.nextElementSibling;
-        if (body.style.display === "block") {
-            body.style.display = "none";
-        } else {
-            body.style.display = "block";
-        }
-    });
+    var body = this.nextElementSibling;
+    if (body.style.display === "block") {
+      body.style.display = "none";
+  } else {
+      body.style.display = "block";
+    }
+  });
 }
